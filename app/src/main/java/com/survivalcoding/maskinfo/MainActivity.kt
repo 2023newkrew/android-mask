349b2218a56d726922964edd543064036e8af09e
bard.kim <bard.kim@kakaocorp.com>
2023-01-02 12:00:47 +0900

    feat : Recycler Ui 추가, adapter 추가

--- app/src/main/java/com/survivalcoding/maskinfo/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/maskinfo/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.survivalcoding.maskinfo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.survivalcoding.maskinfo.adapter.MaskStockAdapter
+import com.survivalcoding.maskinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dummyList = ArrayList<MaskStock>()
+        dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 150))
+        dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 29))
+        dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 30))
+
+        val maskStockRecyclerView = binding.maskStockRecyclerView
+        val maskStockAdapter = MaskStockAdapter(this, dataset = dummyList)
+        maskStockRecyclerView.adapter = maskStockAdapter
+        val linearLayoutManager = LinearLayoutManager(this)
+        maskStockRecyclerView.layoutManager = linearLayoutManager
+
     }
 }
---
+++ app/src/main/java/com/survivalcoding/maskinfo/MaskStock.kt
@@ -0,0 +1,3 @@
+package com.survivalcoding.maskinfo
+
+data class MaskStock(val place: String, val address: String, val distance: Int, val maskAmount: Int)
