769e5906b210ecd5a4d0329c9b094ed446b83ee7
allen <32155219+s45000@users.noreply.github.com>
2023-01-02 14:48:01 +0900

    fix : recycler view UI 수정 및 오류 해결

--- app/src/main/java/com/survivalcoding/maskinfo/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/maskinfo/MainActivity.kt
@@ -14,18 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val dummyList = ArrayList<MaskStock>()
-        dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 150))
+        dummyList.add(MaskStock("판교 아지트", "성남시 분당구 판교로 판교역 동편 테스트", 5000, 150))
         dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 29))
         dummyList.add(MaskStock("판교 아지트", "성남시 분당구", 5000, 30))
 
         val maskStockRecyclerView = binding.maskStockRecyclerView
         val maskStockAdapter = MaskStockAdapter(this, dataset = dummyList)
         maskStockRecyclerView.adapter = maskStockAdapter
-        val linearLayoutManager = LinearLayoutManager(this)
-        maskStockRecyclerView.layoutManager = linearLayoutManager
 
+        binding.toolbar.title = getString(R.string.title, dummyList.size)
     }
 }
