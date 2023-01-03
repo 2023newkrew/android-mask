d2fb75ab31125620dc692ddaee6b50cc85db4b6e
comseung18 <40685565+comseung18@users.noreply.github.com>
2023-01-03 15:33:42 +0900

    refactor: databinding & viewbinding 써서 adapter 리펙토링

--- app/src/main/java/com/survivalcoding/maskinfo/MaskStock.kt
+++ app/src/main/java/com/survivalcoding/maskinfo/MaskStock.kt
@@ -1,3 +1,25 @@
 package com.survivalcoding.maskinfo
 
-data class MaskStock(val place: String, val address: String, val distance: Int, val maskAmount: Int)
+data class MaskStock(
+    val place: String,
+    val address: String,
+    val distance: Int,
+    val status: MaskStockStatus
+){
+    fun statusString(): String = when(status){
+        MaskStockStatus.InSufficient-> "소진임박"
+        MaskStockStatus.Spare -> "여유"
+        MaskStockStatus.Sufficient -> "충분"
+    }
+
+    fun statusColor(): Int= when(status){
+        MaskStockStatus.InSufficient-> R.color.insufficient_red
+        MaskStockStatus.Spare -> R.color.spare_yellow
+        MaskStockStatus.Sufficient -> R.color.sufficient_green
+    }
+    fun amountString(): String = when(status){
+        MaskStockStatus.InSufficient-> "30개 미만"
+        MaskStockStatus.Spare -> "30개 이상"
+        MaskStockStatus.Sufficient -> "100개 이상"
+    }
+}
---
+++ app/src/main/java/com/survivalcoding/maskinfo/MaskStockStatus.kt
@@ -0,0 +1,5 @@
+package com.survivalcoding.maskinfo
+
+enum class MaskStockStatus {
+    InSufficient, Spare, Sufficient
+}
